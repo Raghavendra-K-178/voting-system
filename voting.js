8007479b736bd51af958ce5a70da6658bddf6a45
Your Name <your-email@gmail.com>
2025-12-04 22:20:11 +0530

    Use Aiven MySQL in Render

--- voting.js
+++ voting.js
@@ -116,14 +116,15 @@ a { color:white; font-weight:600; }
 // ---------------- MYSQL CONNECTION ----------------
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: true
+    rejectUnauthorized: false
   }
 });
+
 
 db.connect((err) => {
   if (err) {
